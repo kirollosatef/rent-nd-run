a8bf533e5213316404d7b9c8cf34af05394c53fd
minasame <minasame3@gmail.com>
2022-12-30 00:54:54 +0200

    new is react

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import 'bootstrap5/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+  return (
+    <button className='btn btn-primary'>fhiurhg</button>
+  )
+}
+export default Navbar
